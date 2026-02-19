c8dc0d66f201bf902edcb7499a9e7c33ce0902af
Tidiane-junior <77800865+Tidiane-junior@users.noreply.github.com>
2026-02-19 15:07:05 +0100

    gold dans branche secondaire

---
+++ sql/gold/01_init.sql
@@ -0,0 +1,2 @@
+-- Ce script SQL initialise le schéma "gold" et crée les tables de faits et de dimensions nécessaires pour l'analyse des données. Il inclut également la création d'index pour améliorer les performances des requêtes.
+CREATE SCHEMA IF NOT EXISTS gold;
