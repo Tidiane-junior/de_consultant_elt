84da2961073dc4c8e4fe2c748b69b206ab04a43a
Tidiane-junior <77800865+Tidiane-junior@users.noreply.github.com>
2026-02-19 15:42:40 +0100

    Corrections

--- sql/gold/01_init.sql
+++ sql/gold/01_init.sql
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-=======
--- Ce script SQL initialise le schéma "gold" et crée les tables de faits et de dimensions nécessaires pour l'analyse des données. Il inclut également la création d'index pour améliorer les performances des requêtes.
->>>>>>> dev
+-- On initialise le schéma "gold" et crée les tables de faits et de dimensions nécessaires pour l'analyse des données. 
 CREATE SCHEMA IF NOT EXISTS gold;
